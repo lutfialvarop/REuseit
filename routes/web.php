7199764f9df5e28c089ef242b04aa30a673589ef
lutfialvarop <lutfialvarop@gmail.com>
2025-01-02 20:41:32 +0700

    Add authentication routes and views; update dependencies

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function () {
+    Route::get('login', [AuthController::class, 'login']);
+    Route::get('register', [AuthController::class, 'register']);
 });
