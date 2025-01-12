0ecd3499dedd9dd41f5c6d43d48f5089c6ad0056
Lutfi Alvaro Pratama <lutfialvarop@gmail.com>
2025-01-12 21:34:42 +0700

    Add sidebar functionality and fullscreen toggle for dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,62 @@
-import './bootstrap';
+import "./bootstrap";
+
+if (window.location.href.includes("dashboard")) {
+    // start: Sidebar
+    const sidebarToggle = document.querySelector(".sidebar-toggle");
+    const sidebarOverlay = document.querySelector(".sidebar-overlay");
+    const sidebarMenu = document.querySelector(".sidebar-menu");
+    const main = document.querySelector(".main");
+    if (window.innerWidth < 768) {
+        main.classList.add("active");
+        sidebarOverlay.classList.add("hidden");
+        sidebarMenu.classList.add("-translate-x-full");
+    }
+
+    sidebarToggle.addEventListener("click", function (e) {
+        e.preventDefault();
+        main.classList.toggle("active");
+        sidebarOverlay.classList.toggle("hidden");
+        sidebarMenu.classList.toggle("-translate-x-full");
+    });
+    sidebarOverlay.addEventListener("click", function (e) {
+        e.preventDefault();
+        main.classList.add("active");
+        sidebarOverlay.classList.add("hidden");
+        sidebarMenu.classList.add("-translate-x-full");
+    });
+    document
+        .querySelectorAll(".sidebar-dropdown-toggle")
+        .forEach(function (item) {
+            item.addEventListener("click", function (e) {
+                e.preventDefault();
+                const parent = item.closest(".group");
+                if (parent.classList.contains("selected")) {
+                    parent.classList.remove("selected");
+                } else {
+                    document
+                        .querySelectorAll(".sidebar-dropdown-toggle")
+                        .forEach(function (i) {
+                            i.closest(".group").classList.remove("selected");
+                        });
+                    parent.classList.add("selected");
+                }
+            });
+        });
+    // end: Sidebar
+
+    // Fullscreen button
+    const fullscreenButton = document.getElementById("fullscreen-button");
+
+    fullscreenButton.addEventListener("click", toggleFullscreen);
+
+    function toggleFullscreen() {
+        if (document.fullscreenElement) {
+            // If already in fullscreen, exit fullscreen
+            document.exitFullscreen();
+        } else {
+            // If not in fullscreen, request fullscreen
+            document.documentElement.requestFullscreen();
+        }
+    }
+    // end: Fullscreen button
+}
